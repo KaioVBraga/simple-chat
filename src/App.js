c4253ea0bbe4e1e42a873702ba27f5a4be39391d
kaiovbraga <kaiovbraga2001@gmail.com>
2021-04-18 18:19:36 -0300

    CSS

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import { useState } from 'react';
+import { useRef, useState } from "react";
 
-import './App.css';
+import "./App.css";
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp({
   //your values
-})
+});
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
-const ChatMessage = props => {
+const ChatMessage = (props) => {
   const { text, uid, photoURL } = props.message;
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
-  return <div className={`message ${messageClass}`}>
-    <img src={photoURL} />
-    <p>
-    {text}
-  </p>  
-  </div>
-}
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{text}</p>
+    </div>
+  );
+};
 
 const ChatRoom = () => {
-  const [formValue, setFormValue] = useState('');
+  const [formValue, setFormValue] = useState("");
+  const dummy = useRef();
 
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
 
-  const [messages] = useCollectionData(query, {idField: 'id'});
+  const [messages] = useCollectionData(query, { idField: "id" });
 
-
-  const sendMessage = async e => {
+  const sendMessage = async (e) => {
     e.preventDefault();
 
     const { uid, photoURL } = auth.currentUser;
@@ -46,50 +46,59 @@ const ChatRoom = () => {
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
-    })
+      photoURL,
+    });
 
-    setFormValue('');
-  }
+    setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-  return <>
-    <div>
-      {messages && messages.map(message => <ChatMessage key={messages.id} message={message} />)}
-    </div>
-    <form onSubmit={sendMessage}>
-      <input value={formValue} onChange={e => setFormValue(e.target.value)} />
-      <button type="submit">Send</button>
-    </form>
-    <SignOut/>
-  </>
-}
+  return (
+    <>
+      <div>
+        {messages &&
+          messages.map((message) => (
+            <ChatMessage key={messages.id} message={message} />
+          ))}
+
+        <div ref={dummy}></div>
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+      <SignOut />
+    </>
+  );
+};
 
 const SignIn = () => {
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
-  }
-  
-  return <button onClick={signInWithGoogle}>Sign In</button>
-}
+  };
+
+  return <button onClick={signInWithGoogle}>Sign In</button>;
+};
 
 const SignOut = () => {
-  return auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
-}
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+};
 
 const App = () => {
   const [user] = useAuthState(auth);
 
   return (
     <div className="App">
-      <header>
-        
-      </header>
-      <section>
-        {user ? <ChatRoom/> : <SignIn />}
-      </section>
+      <header>{user ? <SignOut /> : null}</header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
-}
+};
 
 export default App;
